62e318a82145dc4241c310e64042ea21e951961c
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-15 14:03:44 -0500

    stopping point before lunch

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+
+
+class Backer
+
+    attr_reader :name  
+
+    def initialize(name)
+        @name= name
+    end
+
+    def back_project(project) 
+        ProjectBacker.new(project, self)
+    end
+end 
